3ed354907ed1f2edffa8224c497c3f90e43d846b
Jimmy Christensen <dusted@dusted.dk>
2013-12-14 19:31:41 +0100

    Trigger build.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
  * To Public License, Version 2, as published by Sam Hocevar. See
  * http://sam.zoy.org/wtfpl/COPYING for more details. */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
